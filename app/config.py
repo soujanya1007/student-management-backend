142b5f6c15a5203fbd5ac86170e556c0f9810008
soujanya1007 <soujanyahpatil@gmail.com>
2026-03-27 16:30:09 +0530

    Fix database path for Render

--- app/config.py
+++ app/config.py
@@ -1,7 +1,5 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../instance/sms.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'sms.db')
